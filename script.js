2d84e8c92bf73561da16b9f487df74f713e3cc3e
Vamsi <vamsitupakula474@gmail.com>
2021-11-18 21:11:50 +0530

    reset and calculate func added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let screen = document.querySelector(".screen");
+screen.value = "";
 
 function update(selected){
-    screen.value += selected.id
+    screen.value += selected.id;
+}
+
+function calculate(){
+    if(screen.value == ""){
+        return;
+    }
+    screen.value = eval(screen.value);
+}
+
+function reset(){
+    screen.value = "";
 }
